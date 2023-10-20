9b9dca5b3cf003a620a07e81c0a2166e60c4b59a
carinakn <carinaknics@gmail.com>
2023-10-20 18:32:29 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+countDown(5);
