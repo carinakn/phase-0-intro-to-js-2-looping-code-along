6f46f60c63e4ca70d78a2590eb90f379742e5154
carinakn <carinaknics@gmail.com>
2024-09-24 14:30:49 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 function writeCards(names, occasion) {
-    let messages = [];
-  
-    for (let i = 0; i < names.length; i++) {
-      let message = `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`;
-      messages.push(message);
-    }
-  
-    return messages;
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
   }
-  
-  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+  return messages;
+}
 
-function countDown(number) {
-    for (let i = number; i >= 0; i--) {
-      console.log(i);
-    }
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--; // Decrement num by 1
   }
-  
-countDown(5);
+}
